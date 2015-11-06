943ef51934a12627020b4d97df9e189db2f438e7
Jo Tae Sang <gsts007@naver.com>
2015-11-06 22:37:28 +0900

    write_dummy, read_dummy to sqliteio.c, sqlite.h

---
+++ sqliteio.c
@@ -0,0 +1,57 @@
+#include <stddef.h>
+#include "util.h"
+#include "dir.h"
+
+void write_sqlite_file(struct mfs_volume* volume, char* route, char* file_name, char* buff, u32_t offset, u32_t total_size)
+{
+	struct mfs_dirent dentry;
+
+	__mfs_create(volume, route, file_name);
+
+	u128 root_cluster_number = get_cluster_number(volume, route);
+	get_dentry(volume, root_cluster_number, file_name, &dentry);
+
+	while(offset < total_size)
+	{
+		offset += write_file(volume, &dentry, &buff[offset], total_size, offset);
+	}
+
+	alloc_new_entry(volume, root_cluster_number, file_name, &dentry);
+}
+
+void read_sqlite_file(struct mfs_volume* volume, char* route, char* file_name)
+{
+	char* buff;
+	int n_read = 0;
+	u128 cluster_number;
+	struct mfs_dirent dentry;
+	int i;
+
+	int offset = 0;
+
+	cluster_number = get_cluster_number(volume, route);
+	get_dentry(volume, cluster_number, file_name, &dentry);
+
+	int len = dentry.size;
+
+	buff = malloc(len);
+	if(buff == NULL){
+		return;
+	}
+
+	printf("File Size: %d %d\n", len);
+
+	while(offset < len)
+	{
+		n_read = read_file(volume, &dentry , &buff[offset], len, offset);
+		offset += n_read;
+	}
+
+	for (i = 0; i < len; ++i)
+	{
+		printf("%02X ", buff[i] & 0xff);
+		if((i+1)%32 == 0) printf("\n");
+	}
+
+	free(buff);
+}
---
+++ sqliteio.h
@@ -0,0 +1,4 @@
+#include "util.h"
+
+void write_sqlite_file(struct mfs_volume*, char* route, char*, char*, u32_t, u32_t);
+void read_sqlite_file(struct mfs_volume*, char* route, char*);
